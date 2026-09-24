26a36571278a1347ad4c253682406f1c103652f7
bhavinaa <bhavina.sk.04@gmail.com>
2026-09-24 23:48:13 +0800

    fix server side formatting errors

--- server/lattice/db/models/__init__.py
+++ server/lattice/db/models/__init__.py
@@ -1,8 +1,8 @@
 """Table definitions. Importing this package registers every table on `Base.metadata`."""
 
 from lattice.db.models.conversation import Feedback, Session, Turn
-from lattice.db.models.identity import Course, Enrolment, Invite, User
 from lattice.db.models.course_summary import CourseSummary
+from lattice.db.models.identity import Course, Enrolment, Invite, User
 from lattice.db.models.material import Material, ReadingPosition, Topic
 from lattice.db.models.note import Note
 from lattice.db.models.quiz import Quiz, QuizAnswer, QuizQuestion
